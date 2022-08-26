1a79af2734a0b3ee8a0129fb135c37e79967fb00
kattla27 <91956730+kattla27@users.noreply.github.com>
2022-08-26 14:26:23 +0200

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! What is your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hi " + name + " how are you today?");
+        }
+    }
+}
